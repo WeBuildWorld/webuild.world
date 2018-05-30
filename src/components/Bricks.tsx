c82f3f4571bd039996781a6da66d93ef4a0eb0ff
Jerome Chen <jerome@secondcompany.nl>
2018-05-31 02:20:13 +0800

    Show mock bricks.

--- src/components/Bricks.tsx
+++ src/components/Bricks.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { IBrick } from '../types';
+import Brick from './_shared/Brick';
 import './Bricks.css';
 
 export interface IProps {
@@ -9,11 +10,16 @@ export interface IProps {
 }
 
 export default class Bricks extends React.Component<IProps, object> {
-	public render() {
-		const { brickCount, bricks = 1 } = this.props;
 
-		if (bricks <= 0) {
-			throw new Error('You are here too early. :D');
+	public componentWillMount() {
+		this.props.getBricks();
+	}
+
+	public render() {
+		const { brickCount, bricks } = this.props;
+
+		if (brickCount <= 0) {
+			return this.renderNothing();
 		}
 
 		return (
@@ -21,7 +27,18 @@ export default class Bricks extends React.Component<IProps, object> {
 				<div className="greeting">
 					Total {brickCount} bricks.
 				</div>
+
+				{bricks.length && bricks.map((brick => (<Brick brick={brick} />)))}
 			</div>
 		);
 	}
+
+	private renderNothing() {
+		return (
+			<p className="greeting">
+				You are here too early. :D
+			</p>
+		);
+	}
+
 }
---
+++ src/components/_shared/Brick.tsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+import React from "react";
+import { IBrick } from "../../types";
+
+export interface IProps {
+	brick: IBrick;
+}
+
+export default class Brick extends React.Component<IProps, object> {
+	public render() {
+		const { brick } = this.props;
+
+		if (brick === null) {
+			return (<i>Brick is empty</i>);
+		}
+
+		return (
+			<div className="brick">
+				<h4>{brick.title}</h4>
+				<desc>{brick.description}</desc>
+				{brick.tags && brick.tags.map((tag) => <label key={tag}>{tag}</label>)}
+			</div>
+		);
+	}
+}
